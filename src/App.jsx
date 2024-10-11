1fbe288f57d3b47335c533cc40b6d2ae80924e98
tanishaislam <dev.eva.2006@gmail.com>
2024-10-11 20:24:23 +0600

    food resturent project setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className="btn btn-outline">Default</button>
+<button className="btn btn-outline btn-primary">Primary</button>
+<button className="btn btn-outline btn-secondary">Secondary</button>
+<button className="btn btn-outline btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
